0ce506315dd6b02313b524dd6441c1feaaf7235e
Rajesh-Prodian <121475316+Rajesh-Prodian@users.noreply.github.com>
2023-04-17 14:26:04 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FAB',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
